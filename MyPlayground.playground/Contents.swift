1eac30b1885b78e3c1211ae26d3dbce40de27b4b
kingregi1 <59853870+kingregi1@users.noreply.github.com>
2020-02-25 10:06:17 -1000

    p6

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,24 @@
-import UIKit
+ import UIKit
 
 var str = "Hello, playground"
+ 
+//part 4
+for number in 1...5{
+    print(number)
+}
+
+ //part 5
+ 
+ var sum = 0
+ for number in 1...5{
+    sum+=number
+ print(sum)
+ }
+//Part 6 While Loops
+var i = 1
+ while i<=10 {
+    i=i+1
+    print(i)
+ }
+ 
+
